d1333665749a517d71d6653249032e3e6b5d1656
nlau77 <nlau777@gmail.com>
2017-06-29 11:56:52 -0400

    Testing setting global variables in application controller to make variables available in all controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_localfirst_parameters
+
+  # def set_localfirst_parameters
+  #   @state = "Mass"
+  #   @towns= "Boston"
+  # end
+
 end
